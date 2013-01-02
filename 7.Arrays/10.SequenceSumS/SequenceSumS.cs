a2e41b9aa9151e21fba5a8bb8cde812797de50ca
BlueForeverI <bluef0rever@abv.bg>
2013-01-02 12:10:29 +0200

    Completed Task 10 of Arrays.

---
+++ 7.Arrays/10.SequenceSumS/SequenceSumS.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _10.SequenceSumS
+{
+    class SequenceSumS
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter S: ");
+            int S = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter sequence length: ");
+            int N = int.Parse(Console.ReadLine());
+
+            List<int> numbers = new List<int>();
+            for(int i = 0; i < N; i ++)
+            {
+                Console.Write("Enter element {0}: ", i);
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+
+            List<int> result = new List<int>(); for (int i = 0; i < N; i++)
+            {
+                for (int j = N - 1; j >= i; j--)
+                {
+                    List<int> sequence = new List<int>();
+                    for (int k = i; k <= j; k++)
+                    {
+                        sequence.Add(numbers[k]);
+                    }
+                    
+                    if(sequence.Sum() == S)
+                    {
+                        result = new List<int>(sequence);
+                        break;
+                    }
+                }
+            }
+            
+
+            if(result.Count == 0)
+            {
+                Console.WriteLine("No such sequence");
+            }
+            else
+            {
+                Console.WriteLine("[{0}] -> {1}", string.Join(", ", result), S);
+            }
+        }
+    }
+}
